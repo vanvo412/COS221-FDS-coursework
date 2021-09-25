ece9b568acd370004867a65ca017cf008b321fd1
vanvo412 <vanvo41200@gmail.com>
2021-09-25 11:10:47 +0300

    Initial Commit

---
+++ HW2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW2
+//
+//  Created by Van Vo on 25.09.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
